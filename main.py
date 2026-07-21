4c8cbd5fb6c1dffd9331ae2dc10b0c1b0ea57906
tommyrharper <thomasrobertharper@gmail.com>
2026-07-21 16:30:17 +0100

    feat: hello world

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+from ipywidgets import interact
+
+RADIO = ["vla.a", "vla.b", "vla.c", "vla.d", "vlba", "ngvla-revC", "wsrt", "atca_6a", "meerkat"]
+
 def main():
     print("Hello from toms-gridding-simulation!")
+    
 
 
 if __name__ == "__main__":
